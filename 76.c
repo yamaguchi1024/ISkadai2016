bac72d2f2f57f6b4888925d253b32f1479249554
yamaguchi1024 <yukatkh@yahoo.co.jp>
2016-12-06 16:15:43 +0900

    done 76.c

--- 76.c
+++ 76.c
@@ -3,139 +3,131 @@
 #include<ctype.h>
 #include<string.h>
 
-  char po[]="mama";
+char po[]="\0";
 typedef char *String;
 typedef struct tnode *BTree;
 struct tnode {
-  String key;
-  String value;
-  BTree left;
-  BTree right;
+    String key;
+    String value;
+    BTree left;
+    BTree right;
 };
 
 void btree_free(BTree t)
 {
-  printf("free\n");
-  printf("key: %p\n",t->key);
-  printf("value: %p\n",t->value);
-  printf("BTree: %p\n",t);
-  if((t->right)!=0) free(t->right);
-  if((t->left)!=0) free(t->left);
-  if(t->value !=NULL) free(t->value);
-  if(t->key != NULL) free(t->key);
-  free(t);
+    if((t->right)!=0) free(t->right);
+    if((t->left)!=0) free(t->left);
+    if(t->value !=NULL) free(t->value);
+    if(t->key != NULL) free(t->key);
+    free(t);
 }
 
 
 BTree btree_empty()
 {
-  BTree bt = malloc(sizeof(BTree)+100);
-  bt->value = (char *)malloc(100);
-  bt->key = (char *)malloc(100);
-  strcpy(bt->value,po);
-  printf("empty\n");
-  printf("key: %p\n",bt->key);
-  printf("value: %p\n",bt->value);
-  printf("BTree: %p\n",bt);
-  bt->left = 0;
-  bt->right = 0;
-  return bt;
+    BTree bt = malloc(sizeof(BTree)+100);
+    bt->value = (char *)malloc(100);
+    bt->key = (char *)malloc(100);
+    strcpy(bt->value,po);
+    strcpy(bt->key,po);
+    bt->left = 0;
+    bt->right = 0;
+    return bt;
 }
 int btree_isempty(BTree t)
 {
-  if(t->left == 0 && t->right == 0) return 1;
-  else return 0;
+    if(t->left == 0 && t->right == 0) return 1;
+    else return 0;
 }
 
 BTree btree_insert(String key,String val,BTree t)
 {
-  if(btree_isempty(t))
-  {
-    strcpy(t->key,key);
-    strcpy(t->value,val);
-    t->left = btree_empty();
-    t->right = btree_empty();
-  }
-  else if(!strcmp(t->key,key))
-  {
-    strcpy(t->value,val);
-  }
-  else if(strcmp(t->key,key)<0)
-  {
-    btree_insert(key,val,t->right);
-  }
-  else
-  {
-    btree_insert(key,val,t->left);
-  }
-  return t;
+    if(btree_isempty(t))
+    {
+        strcpy(t->key,key);
+        strcpy(t->value,val);
+        t->left = btree_empty();
+        t->right = btree_empty();
+    }
+    else if(!strcmp(t->key,key))
+    {
+        strcpy(t->value,val);
+    }
+    else if(strcmp(t->key,key)<0)
+    {
+        btree_insert(key,val,t->right);
+    }
+    else
+    {
+        btree_insert(key,val,t->left);
+    }
+    return t;
 }
 BTree btree_delete1(String key,BTree t)
 {
-  btree_free(t->left);
-  btree_free(t->right);
-  t->left = NULL;
-  t->right = NULL;
-  return t;
+    btree_free(t);
+    t = btree_empty();
+    return t;
 }
 
 BTree btree_delete2(String key,BTree t)
 {
-  if(btree_isempty(t->right)) t = t->left;
-  else t = t->right;
-  return t;
+    if(btree_isempty(t->right)) t = t->left;
+    else t = t->right;
+    return t;
 }
 BTree search_min(BTree t)
 {
-  if(btree_isempty(t->left)) return t;
-  else return search_min(t->left);
+    if(btree_isempty(t->left)) return t;
+    else return search_min(t->left);
 }
 void del_min(BTree min,BTree t)
 {
-  if(t->left == min) t->left = btree_empty();
-  else del_min(min,t->left);
+    if(t->left == min){ btree_free(t->left); t->left = btree_empty();}
+    else del_min(min,t->left);
 }
 BTree btree_delete3(String key,BTree t)
 {
-  BTree min = search_min(t->right);
-  strcpy(t->key,min->key);
-  strcpy(t->value,min->value);
-  del_min(min,t->right);
-  return t;
+    BTree min = search_min(t->right);
+    strcpy(t->key,min->key);
+    strcpy(t->value,min->value);
+    del_min(min,t->right);
+    return t;
 }
 
 BTree btree_delete(String key,BTree t)
 {
-  if(!strcmp(key,t->key))
-  {
-    if(btree_isempty(t->right) && (btree_isempty(t->left)))
+    if(!strcmp(key,t->key))
     {
-      t = btree_delete1(key,t);
+        if(btree_isempty(t->right) && (btree_isempty(t->left)))
+        {
+            btree_delete1(key,t);
+        }
+        else if(btree_isempty(t->right) || (btree_isempty(t->left)))
+        {
+            btree_delete2(key,t);
+        }
+        else btree_delete3(key,t);
     }
-    else if(btree_isempty(t->right) || (btree_isempty(t->left)))
+    else
     {
-      t = btree_delete2(key,t);
+        if(strcmp(t->key,key)<0) btree_delete(key,t->right);
+        else btree_delete(key,t->left);
     }
-    else t = btree_delete3(key,t);
-  }
-  else
-  {
-    if(strcmp(t->key,key)<0) btree_delete(key,t->right);
-    else btree_delete(key,t->left);
-  }
-  return t;
+    return t;
 
 }
 struct tnode *btree_search(String key,BTree t)
 {
-  if(!strcmp(key,t->key))
-  {
-    return t;
-  }else
-  {
-    if(strcmp(key,t->key)>0) return btree_search(key,t->right);
-    else return btree_search(key,t->left);
-  }
+    if(btree_isempty(t)) return t;
+    else if(!strcmp(key,t->key))
+    {
+        return t;
+    }else
+    {
+        if(strcmp(key,t->key)>0) return btree_search(key,t->right);
+        else return btree_search(key,t->left);
+    }
 }
 
 char quit[]="quit";
@@ -144,29 +136,30 @@ char search[]="search";
 char delete[]="delete";
 int main(int argc,char *argv[])
 {
-  BTree bt;
-  char cmd[20];
-  char key[20];
-  char val[20];
+    BTree bt;
+    char cmd[20];
+    char key[20];
+    char val[20];
 
-  bt = btree_empty();
+    bt = btree_empty();
 
-  while((scanf("%19s",cmd) !=EOF) && (strcmp(quit,cmd))){
-    if(!strcmp(insert,cmd)){
-      scanf("%19s",key);
-      scanf("%19s",val);
-      bt = btree_insert(key,val,bt);
+    while((scanf("%19s",cmd) !=EOF) && (strcmp(quit,cmd))){
+        if(!strcmp(insert,cmd)){
+            scanf("%19s",key);
+            scanf("%19s",val);
+            bt = btree_insert(key,val,bt);
+        }
+        else if(!strcmp(delete,cmd)){
+            scanf("%19s",key);
+            bt =  btree_delete(key,bt);
+        }
+        else if(!strcmp(search,cmd)){
+            scanf("%19s",key);
+            struct tnode *res = btree_search(key,bt);
+            if(!strcmp(res->value,po)) printf("(not found)\n");
+            else printf("%s\n",res->value);
+        }
     }
-    else if(!strcmp(delete,cmd)){
-      scanf("%19s",key);
-      bt =  btree_delete(key,bt);
-    }
-    else if(!strcmp(search,cmd)){
-      scanf("%19s",key);
-      struct tnode *res = btree_search(key,bt);
-      printf("%s\n",res->value);
-    }
-  }
-  btree_free(bt);
-  return 0;
+    btree_free(bt);
+    return 0;
 }
